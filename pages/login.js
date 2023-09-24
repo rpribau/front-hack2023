1e99c96fd0f113001d7e8c5d334838c5ab8213b0
Eduardo Pineda <edpinedaguerra@gmail.com>
2023-09-24 04:36:30 -0600

    feature: Save user input in chat

---
+++ pages/login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Form from "@/components/Form";
+
+
+function Login() {
+
+    return (
+        <div className="flex justify-center items-center w-screen h-screen">
+            <Form />
+        </div>
+    )
+}
+
+export default Login;
